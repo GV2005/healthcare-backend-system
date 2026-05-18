7b7a761a0e535f2ee46428b6f62637e96f4727ff
GV2005 <girivenkatesh2005@gmail.com>
2026-05-18 19:25:31 +0530

    temp fix to local db

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,10 +1,12 @@
 import psycopg2
 
-conn=psycopg2.connect(
-        database="healthcare_db",
-        user="postgres",
-        password="girig0131@G",
-        host="localhost",
-        port="5432"
-)
-cursor=conn.cursor()
+# conn=psycopg2.connect(
+#         database="healthcare_db",
+#         user="postgres",
+#         password="girig0131@G",
+#         host="localhost",
+#         port="5432"
+# )
+# cursor=conn.cursor()
+conn=None
+cursor=None
