b2a15801664f6927f39a90df220a20175f4fa67d
GV2005 <girivenkatesh2005@gmail.com>
2026-05-18 19:57:35 +0530

    fixed cloud db url

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,6 +1,6 @@
 import psycopg2
 import os
 
-database_url = os.getenv("postgresql://gv2005:sFOQlKIOKINUU5n4g6gQEH7aqTxo2a3C@dpg-d85ho4j7uimc739hkgq0-a/healthcare_db_v0no")
+database_url = os.getenv("DATABASE_URL")
 conn=psycopg2.connect(database_url)
 cursor=conn.cursor()
